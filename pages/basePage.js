87c070dd8eb9d4ac22d666022d8f03ed9f80a35d
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-06-04 13:35:29 +0600

    fileCreate

--- pages/basePage.js
+++ pages/basePage.js
@@ -1,10 +1,17 @@
 const { test: baseTest } = require('@playwright/test');
 const LoginPage = require('./loginPage');
+const EditingPage = require('./editingPage');
+
+
 
 const testPages = baseTest.extend({
     loginPage: async ({ page }, use) => {
         await use(new LoginPage(page));
     },
+
+    editingPage: async({page},use)=>{
+        await use(new EditingPage(page));
+    }
 });
 
 const test = testPages;
