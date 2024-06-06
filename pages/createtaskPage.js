012a9765d1e8c334e209ca3122029b672246f487
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-06-06 20:35:08 +0600

    Negative Test: Try to create  Task without name and verify error message "Name is required"

--- pages/createtaskPage.js
+++ pages/createtaskPage.js
@@ -1,46 +1,62 @@
 exports.createtaskPage=class createtaskPage{
 
-constructor(page){
-this.page=page;
-this.addButton ="//span[normalize-space()='Add']";
-this.taskName="//input[@name ='name']";
-this.selectProject="//div[@id='mui-component-select-project']";
-this.projectoption="//li[@role='option']";
-this.selectTag="//div[@id='mui-component-select-tag']";
-this.tagoption="//li[normalize-space()='Research']";
-this.createButton="(//button[normalize-space()='Create'])[1]";
+   constructor(page){
+   this.page=page;
+   this.addButton ="//span[normalize-space()='Add']";
+   this.taskName="//input[@name ='name']";
+   this.selectProject="//div[@id='mui-component-select-project']";
+   this.projectoption="//li[@role='option']";
+   this.selectTag="//div[@id='mui-component-select-tag']";
+   this.tagoption="//li[normalize-space()='Research']";
+   this.createButton="(//button[normalize-space()='Create'])[1]";
+   
+   
+   
+   }
+   
+   async NewTask (Date,Name){
+   await this.page.locator(this.addButton).click();
+   await this.page.getByPlaceholder("DD/MM/YYYY").fill(Date);
+   await this.page.locator(this.taskName).fill(Name);
+   await this.page.waitForTimeout(2000);
+   await this.page.locator(this.selectProject).click();
+   await this.page.locator(this.projectoption).click();
+   await this.page.locator(this.selectTag).click();
+   await this.page.waitForTimeout(2000);
+   await this.page.locator(this.tagoption).click();
+   await this.page.locator(this.createButton).click();
+   await this.page.waitForTimeout(5000);
+   }
+   
+   async verifytaskn(Name) {
+       const Nameoftask = "//h5[normalize-space()='"+Name+"']";
+       return await this.page.locator(Nameoftask ).isVisible()
+    }
+   
+    async verifyproject(SelectProject) {
+       const NameofProject = "(//span[normalize-space()='"+SelectProject+"'])[1]";
+       return await this.page.locator(NameofProject).isVisible()
+    }
+    async verifytag(SelectTag) {
+       const Nameoftag = "(//span[normalize-space()='"+SelectTag+"'])[1]";
+   
+       return await this.page.locator(Nameoftag).isVisible()
+    }
+   
+
+    async CreatTaskwithoutName (Date){
+      await this.page.locator(this.addButton).click();
+      await this.page.getByPlaceholder("DD/MM/YYYY").fill(Date);
+      await this.page.locator(this.selectProject).click();
+      await this.page.locator(this.projectoption).click();
+      await this.page.locator(this.selectTag).click();
+      await this.page.waitForTimeout(2000);
+      await this.page.locator(this.tagoption).click();
+      await this.page.locator(this.createButton).click();
+      await this.page.waitForTimeout(5000);
+      }
 
 
 
-}
 
-async NewTask (Date,Name){
-await this.page.locator(this.addButton).click();
-await this.page.getByPlaceholder("DD/MM/YYYY").fill(Date);
-await this.page.locator(this.taskName).fill(Name);
-await this.page.waitForTimeout(2000);
-await this.page.locator(this.selectProject).click();
-await this.page.locator(this.projectoption).click();
-await this.page.locator(this.selectTag).click();
-await this.page.waitForTimeout(2000);
-await this.page.locator(this.tagoption).click();
-await this.page.locator(this.createButton).click();
-await this.page.waitForTimeout(5000);
-}
-
-async verifytaskn(Name) {
-    const Nameoftask = "//h5[normalize-space()='"+Name+"']";
-    return await this.page.locator(Nameoftask ).isVisible()
- }
-
- async verifyproject(SelectProject) {
-    const NameofProject = "(//span[normalize-space()='"+SelectProject+"'])[1]";
-    return await this.page.locator(NameofProject).isVisible()
- }
- async verifytag(SelectTag) {
-    const Nameoftag = "(//span[normalize-space()='"+SelectTag+"'])[1]";
-
-    return await this.page.locator(Nameoftag).isVisible()
- }
-
-}
+   }
--- tests/createTask.spec.js
+++ tests/createTask.spec.js
@@ -1,14 +1,17 @@
-import { expect, test } from "../pages/basePage";
+
+import { test, expect } from "@playwright/test";
+const LoginPage = require("../pages/loginPage");
 import * as data from "../testData/testData.json";
 import{createtaskPage} from '../pages/createtaskPage';
+const fs = require("fs");
 
-test.describe("Page object test demo", async () => {  
-    let loginPage;
-  test("Login test_02", async ({ page, baseURL, loginPage }) => {
-    await page.goto(`${baseURL}auth/login`);
-    await loginPage.login(data.email, data.password);
-      await page.waitForTimeout(3000);
-
+test.describe("CreateTask", () => {
+  test.beforeEach(async ({ page }) => {
+    const userName = "sumaiya.habib@brainstation-23.com";
+    const password = "BS1574";
+    await page.goto("https://sbueurope.mytask.today/auth/login");
+    const loginPage = new LoginPage(page);
+    await loginPage.login(userName, password);
    const task =new createtaskPage(page);
   await task.NewTask(data.Date,data.Name);
 
@@ -21,11 +24,13 @@ test.describe("Page object test demo", async () => {
   const verifytagkname=await task.verifytag(data.SelectTag);
   await expect(verifytagkname).toBeTruthy();
 
-
-
-
   });
 
+  test("CreateTaskNeg", async ({ page }) => {
+    const task2 =new createtaskPage(page);
+   await task2. CreatTaskwithoutName (data.Date);
+   await expect(page.getByText('Name is required'), 'Name is required').toBeVisible();
+  });
 
 
 })
